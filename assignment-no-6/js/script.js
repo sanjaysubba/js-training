7d625c913f659bf4019ec25b0dfded930f437828
sanjaysubba <sanjay.subba@outside.tech>
2022-05-26 16:30:54 +0545

    tag manager

---
+++ assignment-no-6/js/script.js
@@ -0,0 +1,41 @@
+const canvas = document.getElementById("main");
+
+const ctx = canvas.getContext('2d');
+
+// ctx.moveTo(100, 100);
+// ctx.lineTo(500, 50);
+// ctx.lineTo(600, 250);
+ctx.beginPath();
+ctx.moveTo(100, 20);
+ctx.lineTo(0, 70);
+ctx.lineTo(0, 170);
+ctx.lineTo(15, 170);
+ctx.lineTo(15, 70);
+ctx.lineTo(0, 70);
+ctx.lineTo(100, 70);
+ctx.lineTo(100, 120);
+ctx.lineTo(110, 120);
+ctx.lineTo(110, 70);
+ctx.lineTo(100, 70);
+ctx.lineTo(300, 70);
+ctx.lineTo(300, 170);
+ctx.lineTo(280, 170);
+ctx.lineTo(280, 70);
+ctx.lineTo(300, 70);
+ctx.lineTo(400, 20);
+ctx.lineTo(400, 120);
+ctx.lineTo(380, 120);
+ctx.lineTo(380, 20);
+ctx.lineTo(400, 20);
+ctx.lineTo(100, 20);
+ctx.stroke();
+// ctx.fill();
+
+
+
+
+// ctx.beginPath();
+// ctx.moveTo(575, 550);
+// ctx.lineTo(500, 705);
+// ctx.lineTo(200, 125);
+// ctx.fill();
